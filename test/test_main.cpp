d45c42f033c2d33e521a71c8dfee3861ecc1223e
ekhalabuda <112930468+ekhalabuda@users.noreply.github.com>
2024-02-06 18:47:15 +0300

    Update test_main.cpp

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -11,7 +11,7 @@
 // limitations under the License.
 
 #include <opencv2/ts.hpp>
-
+#include <string>
 #include "algos.hpp"
 
 using namespace cv;
@@ -24,6 +24,7 @@ static const int julia_height = 200;
 
 CV_TEST_MAIN("")
 
+<<<<<<< HEAD
 TEST(julia, ref) {
     Mat dst(julia_height, julia_width, CV_8UC1, Scalar(0));
 
@@ -38,6 +39,37 @@ TEST(julia, halide) {
     halide_julia(dst.ptr<uint8_t>(), dst.rows, dst.cols);
 
     imwrite("julia_halide.png", dst);
+=======
+TEST(ascii_art_ref, opencv){
+    std::string grey_scale = "$@B%8&WM#*OAHKDPQWMRZO0QLCJUYXVJFT/|()1{}[]?-_+~<>i!lI;:,\"^`'.  ";
+    for(int i = 0; i < grey_scale.size(); ++i){
+        cv::Mat m(20, 15, CV_8U, cv::Scalar(255, 255, 255));
+        std::string s(1,grey_scale[i] );
+        cv::putText(m, s, cv::Point(1, 15),cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 1);
+        std::string filename = s + ".png";
+        cv::imwrite(filename, m);
+        
+    }
+
+    ASSERT_EQ(true, true);
+}
+TEST(test, opencv){
+    Mat src(height, width, CV_8UC1), dst(height, width, CV_8U), ref(height, width, CV_8U);
+    randu(src, 0, 256);
+    ascii_art_ref(src.ptr<uint8_t>(), dst.ptr<uint8_t>(), src.rows, src.cols);
+    std::string s = "";
+    for(size_t j=0; j<dst.cols; j++){
+        for(size_t i = 0; i < dst.rows; i++){
+            s+=std::to_string(dst.at<uint8_t>(j,i));
+        }
+    }
+    std::cout<<s;
+
+    imwrite("src_ascii.png", src);
+    cv::putText(dst, s, cv::Point(100, 20),cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 1);
+    imwrite("res_ascii.png", dst);
+    ASSERT_EQ(true, true);
+>>>>>>> 3f62d2b (Update test_main.cpp)
 }
 
 TEST(histogram, opencv) {
@@ -77,7 +109,9 @@ TEST(bgr2gray_interleaved, halide) {
     randu(src, 0, 256);
 
     bgr2gray_interleaved_halide(src.ptr<uint8_t>(), dst.ptr<uint8_t>(), src.rows, src.cols);
-    bgr2gray_ref(src.ptr<uint8_t>(), ref.ptr<uint8_t>(), src.rows, src.cols);
+    imwrite("src.png", src);
+    imwrite("res.png", dst);
+    // bgr2gray_ref(src.ptr<uint8_t>(), ref.ptr<uint8_t>(), src.rows, src.cols);
 
     ASSERT_LE(norm(ref, dst, NORM_INF), 0);
 }
