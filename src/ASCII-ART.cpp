16654abf6a1111dea0928aeab94cd49dbd1ddda2
ekhalabuda <112930468+ekhalabuda@users.noreply.github.com>
2024-02-07 08:24:34 +0300

    Update ASCII-ART.cpp

--- src/ASCII-ART.cpp
+++ src/ASCII-ART.cpp
@@ -13,7 +13,6 @@ using namespace Halide::Runtime;
 
 #include <Halide.h>
 
-using namespace Halide;
 
 
 #endif
@@ -26,46 +25,74 @@ static const int ry = 19;
     Buffer<uint16_t> input(src, {width, height});
     Buffer<uint16_t> output(dst, {width - 2, height - 2});
 }*/
+/*void ascii_art_halide(const uint8_t* src, uint8_t* dst, int input_height, int input_width) {
+    Halide::Func ascii_art;
+    Halide::Var x, y;
+    int output_width = input_width / rx;
+    int output_height = input_height / ry;
 
-void ascii_art_ref(const uint8_t* src, uint8_t* dst, int height, int width) {
-    //convert to grey
+    // Define input buffer
+    Halide::Buffer<uint8_t> input(src, input_width, input_height);
 
-    std::vector<uint8_t> averanges = {};
-    //parse to windows with size rx*ry
+    // Convert to grey
+    Halide::Func grey;
+    grey(x, y) = (input(x, y, 0) + input(x, y, 1) + input(x, y, 2)) / 3;
 
-    float lum = 0;
-    for(int y = 0; y <= height - ry; y+=ry){
-        for(int x = 0; x < width; x+=rx){
-            
-            for(int j = y; j < y + ry; ++j){
-                for(int i = x; i < x + rx; ++i){
-                    lum += src[ j * width + i];
-                    
-                }
-                
-            }
-            
-            lum/=rx*ry;
-            averanges.push_back(static_cast<uint8_t>(lum));
-            lum = 0;
+
+    // Parse to windows with size rx*ry
+    Halide::Func lum;
+    Halide::RDom r(0, rx, 0, ry);
+    lum(x, y) += grey(x * rx + r.x, y * ry + r.y);
+
+    // Calculate average luminance
+    Halide::Func average_lum;
+    average_lum(x, y) = lum(x, y) / (rx * ry);
+
+    // Define grey scale characters
+    const std::string grey_scale = "$@B%8&WM#*OAHKDPQWMRZO0QLCJUYXVJFT/|()1{}[]?-_+~<>i!lI;:,^`'.  ";
+    uint8_t tmp = 255.0 / grey_scale.size();
+
+    // Map average luminance to grey scale characters
+    Halide::Func output;
+    output(x, y) = grey_scale[Halide::cast<int>(average_lum(x, y) / tmp)];
+
+    // Evaluate the output function
+    Halide::Buffer<uint8_t> output_buffer = output.realize(output_width, output_height);
+
+    // Copy the result to the output buffer
+    for (int y = 0; y < output_height; y++) {
+        for (int x = 0; x < output_width; x++) {
+            dst[y * output_width + x] = output_buffer(x, y);
         }
-
-        
     }
-    
-    std::string grey_scale = "$@B%8&WM#*OAHKDPQWMRZO0QLCJUYXVJFT/|()1{}[]?-_+~<>i!lI;:,^`'.  ";
-    std::vector<uint8_t> result = {};
+}*/
+void ascii_art_ref(const uint8_t* src, uint8_t* dst, int input_height, int input_width) {
+    //convert to grey
+    //...
+    //parse to windows with size rx*ry
+    int output_width = input_width / rx;
+    int output_height = input_height / ry;
+    for(int y = 0; y < output_height; y++){
+        for(int x = 0; x < output_width; x++){
+            float lum = 0;
+            for(int j = 0; j < ry; ++j){
+                for(int i = 0; i < rx; ++i){
+                    lum += src[ (y * ry + j) * input_width + x * rx +i];   
+                }   
+            }
+            dst[y * output_width + x] = static_cast<uint8_t>(lum/(rx*ry));
+            lum = 0;
+        }   
+    }
+    const std::string grey_scale = "$@B%8&WM#*OAHKDPQWMRZO0QLCJUYXVJFT/|()1{}[]?-_+~<>i!lI;:,^`'.  ";
     uint8_t tmp = 255.0/ grey_scale.size();
 
-    for(int i = 0; i < averanges.size(); ++i){
-        result.push_back(grey_scale[ floor(averanges[i]/tmp)]);
-    
+
+    for(int y = 0; y < output_height; y++){
+        for(int x = 0; x < output_width; x++){
+            dst[y * output_width + x] = grey_scale[ floor(dst[y * output_width + x]/tmp)];
+        }   
     }
-    /*
-    for(int j = 0; j < height/ry; j++)
-        for(int i = 0; i < width/rx; ++i) 
-            dst[i+j*width/rx] = result[i][j];
-    */
 
 
 }
