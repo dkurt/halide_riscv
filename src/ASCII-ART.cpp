d513a902797d5c5e93316b0d1995674ff9eefc33
ekhalabuda <112930468+ekhalabuda@users.noreply.github.com>
2024-02-07 17:58:20 +0300

    Update ASCII-ART.cpp

--- src/ASCII-ART.cpp
+++ src/ASCII-ART.cpp
@@ -6,6 +6,7 @@
 
 #ifdef __riscv
 #include <HalideBuffer.h>
+#include "ascii_art.h"
 
 using namespace Halide::Runtime;
 
@@ -13,48 +14,61 @@ using namespace Halide::Runtime;
 
 #include <Halide.h>
 
-
+using namespace Halide;
 
 #endif
 static const int norm_w = 3;
 static const int norm_h = 3;
-static const int rx = 5;
-static const int ry = 9;
-/*
-void ascii_art_halide(const uint8_t* src, uint8_t* dst, int input_height, int input_width) {
-    Halide::Func ascii_art;
-    Halide::Var x, y;
+static const int rx = 15;
+static const int ry = 19;
+
+void ascii_art_halide(uint8_t* src, uint8_t* dst, int input_height, int input_width) {
     int output_width = input_width / rx;
     int output_height = input_height / ry;
+ 
+    
+    Buffer<uint8_t> input(src, {input_width, input_height});
+    Buffer<uint8_t> output(dst, {input_width / rx, input_height / ry});
+#ifdef __riscv
+    ascii_art(input, output);
+#else
+    static Func ascii("ascii_art");
+    if (!ascii.defined()) {
+        // Func padded = BoundaryConditions::constant_exterior(input, 0);
 
-    Halide::Buffer<uint8_t> input(src, input_width, input_height);
+        Var x("x"), y("y");
+        RDom r(0, rx, 0, ry);
+        Expr s = sum(cast<uint32_t>(input(x*rx + r.x, y*ry + r.y)));
+    
+        //s = Halide::clamp(s/(rx*ry),0,255);
+        ascii(x, y) = cast<uint8_t>(s/(rx*ry));
+        // ascii.realize(output);
+        // Compile
+        Target target;
+        target.os = Target::OS::Linux;
+        target.arch = Target::Arch::RISCV;
+        target.bits = 64;
+        //target.vector_bits = factor * sizeof(uint8_t) * 8;
 
-    // Convert to grey
- //   Halide::Func grey;
-   // grey(x, y) = (input(x, y, 0) + input(x, y, 1) + input(x, y, 2)) / 3;
+        // Tested XuanTie C906 has 128-bit vector unit
+        CV_Assert(target.vector_bits <= 128);
 
-    Halide::Func lum;
-    Halide::RDom r(0, rx, 0, ry);
-    lum(x, y) += grey(x * rx + r.x, y * ry + r.y);
+        std::vector<Target::Feature> features;
+        features.push_back(Target::RVV);
+        features.push_back(Target::NoAsserts);
+        features.push_back(Target::NoRuntime);
+        target.set_features(features);
 
-    Halide::Func average_lum;
-    average_lum(x, y) = lum(x, y) / (rx * ry);
+        std::cout << target << std::endl;
+        ascii.print_loop_nest();
 
-    const std::string grey_scale = "$@B%8&WM#*OAHKDPQWMRZO0QLCJUYXVJFT/|()1{}[]?-_+~<>i!lI;:,^`'.  ";
-    uint8_t tmp = 255.0 / grey_scale.size();
-    Halide::Func output;
-    output(x, y) = grey_scale[Halide::cast<int>(average_lum(x, y) / tmp)];
-    Halide::Buffer<uint8_t> output_buffer = output.realize(output_width, output_height);
-    for (int y = 0; y < output_height; y++) {
-        for (int x = 0; x < output_width; x++) {
-            dst[y * output_width + x] = output_buffer(x, y);
-        }
+        // Dump AOT code
+        ascii.compile_to_header("ascii_art.h", {input}, "ascii_art", target);
+        ascii.compile_to_assembly("ascii_art.s", {input}, "ascii_art", target);
     }
-}*/
+    #endif
+}
 void ascii_art_ref(const uint8_t* src, uint8_t* dst, int input_height, int input_width) {
-    //convert to grey
-    //...
-    //parse to windows with size rx*ry
     int output_width = input_width / rx;
     int output_height = input_height / ry;
     for(int y = 0; y < output_height; y++){
@@ -69,15 +83,4 @@ void ascii_art_ref(const uint8_t* src, uint8_t* dst, int input_height, int input
             lum = 0;
         }   
     }
-    const std::string grey_scale = "$@B%8&WM#*OAHKDPQWMRZO0QLCJUYXVJFT/|()1{}[]?-_+~<>i!lI;:,^`'.  ";
-    uint8_t tmp = 255.0/ grey_scale.size();
-
-
-    // for(int y = 0; y < output_height; y++){
-    //     for(int x = 0; x < output_width; x++){
-    //         dst[y * output_width + x] = grey_scale[ floor(dst[y * output_width + x]/tmp)];
-    //     }   
-    // }
-
-
 }
