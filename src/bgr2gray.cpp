7906db883d6b17096109210caeafcb0ef6d0db56
ekhalabuda <112930468+ekhalabuda@users.noreply.github.com>
2024-02-07 20:06:10 +0300

    Update bgr2gray.cpp

--- src/bgr2gray.cpp
+++ src/bgr2gray.cpp
@@ -28,36 +28,6 @@ using namespace Halide;
 #endif
 
 
-void bgr2gray_interleaved_halide(uint8_t* src,// Copyright (C), 2023, KNS Group LLC (YADRO)
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//     http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-#include <opencv2/opencv.hpp>
-
-#ifdef __riscv
-#include <HalideBuffer.h>
-#include "bgr2gray_interleaved.h"
-#include "bgr2gray_planar.h"
-
-using namespace Halide::Runtime;
-
-#else
-
-#include <Halide.h>
-
-using namespace Halide;
-
-#endif
-
-
 void bgr2gray_interleaved_halide(uint8_t* src, uint8_t* dst, int height, int width) {
     auto input = Buffer<uint8_t>::make_interleaved(src, width, height, 3);
     Buffer<uint8_t> output(dst, {width, height});
@@ -91,151 +61,29 @@ void bgr2gray_interleaved_halide(uint8_t* src, uint8_t* dst, int height, int wid
         int factor = 16;
         f.vectorize(x, factor);
         f.realize(output);
-        // // Compile
-        // Target target;
-        // target.os = Target::OS::Linux;
-        // target.arch = Target::Arch::RISCV;
-        // target.bits = 64;
-        // target.vector_bits = factor * sizeof(uint8_t) * 8;
 
-        // // Tested XuanTie C906 has 128-bit vector unit
-        // CV_Assert(target.vector_bits <= 128);
+    //     // Compile
+    //     Target target;
+    //     target.os = Target::OS::Linux;
+    //     target.arch = Target::Arch::RISCV;
+    //     target.bits = 64;
+    //     target.vector_bits = factor * sizeof(uint8_t) * 8;
 
-        // std::vector<Target::Feature> features;
-        // features.push_back(Target::RVV);
-        // features.push_back(Target::NoAsserts);
-        // features.push_back(Target::NoRuntime);
-        // target.set_features(features);
+    //     // Tested XuanTie C906 has 128-bit vector unit
+    //     CV_Assert(target.vector_bits <= 128);
 
-        // std::cout << target << std::endl;
-        // f.print_loop_nest();
+    //     std::vector<Target::Feature> features;
+    //     features.push_back(Target::RVV);
+    //     features.push_back(Target::NoAsserts);
+    //     features.push_back(Target::NoRuntime);
+    //     target.set_features(features);
 
-        // // Dump AOT code
-        // f.compile_to_header("bgr2gray_interleaved.h", {input}, "bgr2gray_interleaved", target);
-        // f.compile_to_assembly("bgr2gray_interleaved.s", {input}, "bgr2gray_interleaved", target);
-    }
-#endif
-}
+    //     std::cout << target << std::endl;
+    //     f.print_loop_nest();
 
-void bgr2gray_planar_halide(uint8_t* src, uint8_t* dst, int height, int width) {
-    Buffer<uint8_t> input(src, width, height, 3);
-    Buffer<uint8_t> output(dst, {width, height});
-#ifdef __riscv
-    bgr2gray_planar(input, output);
-#else
-    static Func f("bgr2gray");
-    if (!f.defined()) {
-        uint16_t R2GRAY = 77, G2GRAY = 150, B2GRAY = 29;
-
-        Var x("x"), y("y");
-        Expr b = cast<uint16_t>(input(x, y, 0));
-        Expr g = cast<uint16_t>(input(x, y, 1));
-        Expr r = cast<uint16_t>(input(x, y, 2));
-        f(x, y) = cast<uint8_t>((R2GRAY * r + G2GRAY * g + B2GRAY * b) >> 8);
-
-        // Schedule
-        int factor = 16;
-        f.vectorize(x, factor);
-
-        // Compile
-        Target target;
-        target.os = Target::OS::Linux;
-        target.arch = Target::Arch::RISCV;
-        target.bits = 64;
-        target.vector_bits = factor * sizeof(uint8_t) * 8;
-
-        // Tested XuanTie C906 has 128-bit vector unit
-        CV_Assert(target.vector_bits <= 128);
-
-        std::vector<Target::Feature> features;
-        features.push_back(Target::RVV);
-        features.push_back(Target::NoAsserts);
-        features.push_back(Target::NoRuntime);
-        target.set_features(features);
-
-        std::cout << target << std::endl;
-        f.print_loop_nest();
-
-        // Dump AOT code
-        f.compile_to_header("bgr2gray_planar.h", {input}, "bgr2gray_planar", target);
-        f.compile_to_assembly("bgr2gray_planar.s", {input}, "bgr2gray_planar", target);
-    }
-#endif
-}
-
-void bgr2gray_opencv(const cv::Mat& src, cv::Mat& dst) {
-    cv::cvtColor(src, dst, cv::COLOR_BGR2GRAY);
-}
-
-void bgr2gray_ref(const uint8_t* src, uint8_t* dst, int height, int width) {
-    uint16_t red, green, blue, R2GRAY = 77, G2GRAY = 150, B2GRAY = 29;
-
-    for (int y = 0; y < height; ++y) {
-        const uint8_t* srcData = src + y * width * 3;
-        uint8_t* dstData = dst + y * width;
-        for (int x = 0; x < width; ++x) {
-            blue = static_cast<uint16_t>(srcData[x * 3]);
-            green = static_cast<uint16_t>(srcData[x * 3 + 1]);
-            red = static_cast<uint16_t>(srcData[x * 3 + 2]);
-            dstData[x] = static_cast<uint8_t>((R2GRAY * red + G2GRAY * green + B2GRAY * blue) >> 8);
-        }
-    }
-}
- uint8_t* dst, int height, int width) {
-    auto input = Buffer<uint8_t>::make_interleaved(src, width, height, 3);
-    Buffer<uint8_t> output(dst, {width, height});
-#ifdef __riscv
-    bgr2gray_interleaved(input, output);
-#else
-    static Func f("bgr2gray");
-    if (!f.defined()) {
-        input.set_name("input");
-        uint16_t R2GRAY = 77, G2GRAY = 150, B2GRAY = 29;
-
-        Var x("x"), y("y"), c("c");
-        Func planar("planar");
-        planar(x, y, c) = input(x, y, c);
-
-        Expr b = cast<uint16_t>(planar(x, y, 0));
-        Expr g = cast<uint16_t>(planar(x, y, 1));
-        Expr r = cast<uint16_t>(planar(x, y, 2));
-        Expr res = (R2GRAY * r + G2GRAY * g + B2GRAY * b) >> 8;
-        f(x, y) = cast<uint8_t>(res);
-
-        // Schedule
-        f.bound(x, 0, width).bound(y, 0, height);
-
-        planar.output_buffer().dim(0).set_stride(1).set_extent(width);
-        planar.output_buffer().dim(1).set_stride(width).set_extent(height);
-        planar.output_buffer().dim(2).set_stride(width * height).set_extent(3);
-        planar.compute_at(f, y);
-        planar.reorder(c, x, y).unroll(c);
-
-        int factor = 16;
-        f.vectorize(x, factor);
-
-        // Compile
-        Target target;
-        target.os = Target::OS::Linux;
-        target.arch = Target::Arch::RISCV;
-        target.bits = 64;
-        target.vector_bits = factor * sizeof(uint8_t) * 8;
-
-        // Tested XuanTie C906 has 128-bit vector unit
-        CV_Assert(target.vector_bits <= 128);
-
-        std::vector<Target::Feature> features;
-        features.push_back(Target::RVV);
-        features.push_back(Target::NoAsserts);
-        features.push_back(Target::NoRuntime);
-        target.set_features(features);
-
-        std::cout << target << std::endl;
-        f.print_loop_nest();
-
-        // Dump AOT code
-        f.compile_to_header("bgr2gray_interleaved.h", {input}, "bgr2gray_interleaved", target);
-        f.compile_to_assembly("bgr2gray_interleaved.s", {input}, "bgr2gray_interleaved", target);
+    //     // Dump AOT code
+    //     f.compile_to_header("bgr2gray_interleaved.h", {input}, "bgr2gray_interleaved", target);
+    //     f.compile_to_assembly("bgr2gray_interleaved.s", {input}, "bgr2gray_interleaved", target);
     }
 #endif
 }
